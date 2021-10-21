5e57099e20066d14b80325bb3dcfab4ebfdedc63
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-10-21 09:56:41 +0200

    style(prettier): format with prettier

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react'
 export default function useDebounce(value, delay) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-  useEffect(
-    () => {
-      const handler = setTimeout(() => {
-        setDebouncedValue(value);
-      }, delay);
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
 
-      return () => clearTimeout(handler);
-    },
-    [value] 
-  );
+    return () => clearTimeout(handler)
+  }, [value])
 
-  return debouncedValue;
+  return debouncedValue
 }
