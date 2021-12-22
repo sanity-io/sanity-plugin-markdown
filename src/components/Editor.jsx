678bba361b22c5ad72424fb3e70104b96678f41e
Robin Pyon <robin.pyon@gmail.com>
2021-12-22 11:25:14 +0000

    fix: conditionally style textarea based on readOnly access, pass through forwarded ref + onBlur/onFocus callbacks

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import ReactMde from 'react-mde'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
+import {useId} from '@reach/auto-id'
 
 import PatchEvent, {set, unset} from 'part:@sanity/form-builder/patch-event'
 import sanityClient from 'part:@sanity/base/client'
@@ -22,11 +23,21 @@ const defaultToolbarCommands = [
 ]
 
 export default React.forwardRef(function MarkdownEditor(props, ref) {
-  const {type, value = '', markers, presence, readOnly} = props
+  const {type, value = '', markers, onBlur, onChange, onFocus, presence, readOnly} = props
   const {options = {}} = type
   const [selectedTab, setSelectedTab] = React.useState('write')
   const [editedValue, setEditedValue] = React.useState(value)
   const debouncedValue = useDebounce(editedValue, 100)
+  const textarea = React.useRef()
+
+  // Conditionally update textarea styles based on read only access.
+  // It's also possible to define inline styles via `childProps.textArea.style` on `<ReactMde />`,
+  // but this will override any dynamically created styles provided by the component itself.
+  React.useEffect(() => {
+    if (textarea.current) {
+      textarea.current.style.backgroundColor = readOnly ? 'rgba(240,240,240)' : 'rgba(255,255,255)'
+    }
+  }, [textarea])
 
   React.useEffect(() => {
     setEditedValue(value)
@@ -34,9 +45,9 @@ export default React.forwardRef(function MarkdownEditor(props, ref) {
 
   React.useEffect(() => {
     if (!debouncedValue && value) {
-      props.onChange(PatchEvent.from([unset()]))
+      onChange(PatchEvent.from([unset()]))
     } else if (debouncedValue !== value) {
-      props.onChange(PatchEvent.from([set(debouncedValue)]))
+      onChange(PatchEvent.from([set(debouncedValue)]))
     }
   }, [debouncedValue])
 
@@ -56,29 +67,47 @@ export default React.forwardRef(function MarkdownEditor(props, ref) {
     return success
   }
 
+  // Generate a random ID to link our FormField label and input component
+  const inputId = useId()
+
   return (
     <FormField
       description={type.description} // Creates description from schema
+      inputId={inputId} // A unique ID for this input
       title={type.title} // Creates label from schema title
       __unstable_markers={markers} // Handles all markers including validation
       __unstable_presence={presence} // Handles presence avatars
     >
-      <ReactMde
-        toolbarCommands={options['toolbar'] || defaultToolbarCommands}
-        value={editedValue}
-        onChange={setEditedValue}
-        selectedTab={selectedTab}
-        onTabChange={setSelectedTab}
-        readOnly={readOnly}
-        generateMarkdownPreview={(markdown) => Promise.resolve(<Preview markdown={markdown} />)}
-        childProps={{
-          writeButton: {
-            tabIndex: -1,
-          },
-        }}
-        refs={{textarea: ref}}
-        paste={{saveImage}}
-      />
+      {/*
+        Assign our forwarded ref to a wrapper element.
+
+        Ideally, this should be assigned to the textarea inside <ReactMde /> component.
+        Whilst `react-mde` does allow you to target individual sub-components' refs,
+        it will complain if you try and assign a forwarded ref.
+      */}
+      <div ref={ref}>
+        <ReactMde
+          toolbarCommands={options['toolbar'] || defaultToolbarCommands}
+          value={editedValue}
+          onChange={setEditedValue}
+          selectedTab={selectedTab}
+          onTabChange={setSelectedTab}
+          readOnly={readOnly}
+          generateMarkdownPreview={(markdown) => Promise.resolve(<Preview markdown={markdown} />)}
+          childProps={{
+            textArea: {
+              id: inputId,
+              onBlur,
+              onFocus,
+            },
+            writeButton: {
+              tabIndex: -1,
+            },
+          }}
+          paste={{saveImage}}
+          refs={{textarea}}
+        />
+      </div>
     </FormField>
   )
 })
