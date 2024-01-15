e5627c4c68b358c66b1d18017c4e9cf410486622
Robin Pyon <robin.pyon@gmail.com>
2024-01-15 10:28:56 +0000

    fix: use typed theme object and optional chain access for selectable colors

--- src/components/MarkdownInputStyles.tsx
+++ src/components/MarkdownInputStyles.tsx
@@ -1,61 +1,63 @@
-import styled from 'styled-components'
-import {Box} from '@sanity/ui'
+import {Box, type Theme} from '@sanity/ui'
+import styled, {css} from 'styled-components'
 
-export const MarkdownInputStyles = styled(Box)`
-  & .CodeMirror.CodeMirror {
-    color: ${({theme}) => theme.sanity.color.card.enabled.fg};
-    border-color: ${({theme}) => theme.sanity.color.card.enabled.border};
-    background-color: inherit;
-  }
-
-  & .cm-s-easymde .CodeMirror-cursor {
-    border-color: ${({theme}) => theme.sanity.color.card.enabled.fg};
-  }
-
-  & .editor-toolbar,
-  .editor-preview-side {
-    border-color: ${({theme}) => theme.sanity.color.card.enabled.border};
-  }
-
-  & .CodeMirror-focused .CodeMirror-selected.CodeMirror-selected.CodeMirror-selected {
-    background-color: ${({theme}) => theme.sanity.color.selectable.primary.hovered.bg};
-  }
-
-  & .CodeMirror-selected.CodeMirror-selected.CodeMirror-selected {
-    background-color: ${({theme}) => theme.sanity.color.card.enabled.bg};
-  }
-
-  & .editor-toolbar > * {
-    color: ${({theme}) => theme.sanity.color.card.enabled.fg};
-  }
-
-  & .editor-toolbar > .active,
-  .editor-toolbar > button:hover,
-  .editor-preview pre,
-  .cm-s-easymde .cm-comment {
-    background-color: ${({theme}) => theme.sanity.color.card.enabled.bg};
-  }
-
-  & .editor-preview {
-    background-color: ${({theme}) => theme.sanity.color.card.enabled.bg};
-
-    & h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-size: revert;
+export const MarkdownInputStyles = styled(Box)(({theme}: {theme: Theme}) => {
+  return css`
+    & .CodeMirror.CodeMirror {
+      color: ${theme.sanity.color.card.enabled.fg};
+      border-color: ${theme.sanity.color.card.enabled.border};
+      background-color: inherit;
     }
 
-    & ul,
-    li {
-      list-style: revert;
-      padding: revert;
+    & .cm-s-easymde .CodeMirror-cursor {
+      border-color: ${theme.sanity.color.card.enabled.fg};
     }
 
-    & a {
-      text-decoration: revert;
+    & .editor-toolbar,
+    .editor-preview-side {
+      border-color: ${theme.sanity.color.card.enabled.border};
     }
-  }
-`
+
+    & .CodeMirror-focused .CodeMirror-selected.CodeMirror-selected.CodeMirror-selected {
+      background-color: ${theme.sanity.color.selectable?.primary?.hovered?.bg};
+    }
+
+    & .CodeMirror-selected.CodeMirror-selected.CodeMirror-selected {
+      background-color: ${theme.sanity.color.card.enabled.bg};
+    }
+
+    & .editor-toolbar > * {
+      color: ${theme.sanity.color.card.enabled.fg};
+    }
+
+    & .editor-toolbar > .active,
+    .editor-toolbar > button:hover,
+    .editor-preview pre,
+    .cm-s-easymde .cm-comment {
+      background-color: ${theme.sanity.color.card.enabled.bg};
+    }
+
+    & .editor-preview {
+      background-color: ${theme.sanity.color.card.enabled.bg};
+
+      & h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        font-size: revert;
+      }
+
+      & ul,
+      li {
+        list-style: revert;
+        padding: revert;
+      }
+
+      & a {
+        text-decoration: revert;
+      }
+    }
+  `
+})
